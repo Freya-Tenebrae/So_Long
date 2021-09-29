6990b93474d1fa489df83fc4ae8cc067ddc0a33f
Celien MAGINOT <cmaginot@student.42.fr>
2021-09-29 18:08:53 +0200

    add some tile's sprite

--- srcs/ft_game_init.c
+++ srcs/ft_game_init.c
@@ -6,19 +6,11 @@
 /*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 15:27:34 by cmaginot          #+#    #+#             */
-/*   Updated: 2021/09/28 17:49:03 by cmaginot         ###   ########.fr       */
+/*   Updated: 2021/09/29 18:06:18 by cmaginot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_so_long.h"
-
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -28,39 +20,48 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-char	*ft_game_init(char **map)
+void	ft_fill_img(void *mlx, t_data *img)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
+	int i;
+	int j;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "so_long");
-	img.img = mlx_new_image(mlx, 500, 500);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-
-
-	int i = 0;
-	while (i <= 500)
+	img->img = mlx_new_image(mlx, SIZE_SPRITE, SIZE_SPRITE);
+	img->addr = mlx_get_data_addr(img->img, &(img->bits_per_pixel),
+								&(img->line_length), &(img->endian));
+	i = 0;
+	while (i < SIZE_SPRITE)
 	{
-		int j = 0;
-		while (j <= 500)
+		j = 0;
+		while (j < SIZE_SPRITE)
 		{
-			if (i < 10 || i >= 490 || j < 10 || j >= 490)
-				my_mlx_pixel_put(&img, i, j, 0x00FF0000);
+			if (i < 4 || i >= SIZE_SPRITE - 4 || j < 4 || \
+				j >= SIZE_SPRITE - 4)
+				my_mlx_pixel_put(img, i, j, 0x00FF0000);
 			else
-				my_mlx_pixel_put(&img, i, j, 0xFF000000);
+				my_mlx_pixel_put(img, i, j, 0xFF000000);
 			j += 1;
 		}
 		i += 1;
 	}
+}
 
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 100, 100);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 350, 100);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 100, 350);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 350, 350);
+char	*ft_game_init(char **map)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img[4];
+
+	ft_putnbr(SIZE_SPRITE);
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, SIZE_SPRITE * 24, SIZE_SPRITE * 12, "so_long");
+
+	ft_fill_img(mlx, &(img[0]));
+	ft_fill_img(mlx, &(img[1]));
+	ft_fill_img(mlx, &(img[2]));
+	ft_fill_img(mlx, &(img[3]));
 	mlx_loop(mlx);
+
 	(void)map;
 	return ("");
 }
